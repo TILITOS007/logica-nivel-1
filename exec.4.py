f0d1f06bb747cb84cbde661a2cdf7b3ed7184794
Davi Silva <ds452119@gmail.com>
2024-10-03 19:52:27 -0300

    Update exec.4.py

--- exec.4.py
+++ exec.4.py
@@ -1,8 +1,9 @@
 #4. Quadrado - Escrever um programa que imprima a seguinte figura:
 
 
-XXXXX
-X   X
-X   X
-X   X
-XXXXX
+print("XXXXX")
+print("X   X")
+print("X   X")
+print("X   X")
+print("XXXXX")
+
